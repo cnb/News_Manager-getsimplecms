876d9d93a52347bd961b9a8db1628b9c5e2e0d06
cnb <carnav@gmail.com>
2013-05-24 21:16:38 +0200

    version 2.4.2

--- news_manager.php
+++ news_manager.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: News Manager
 Description: A blog/news plugin for GetSimple
-Version: 2.4.2 beta
+Version: 2.4.2
 Author: Rogier Koppejan
 Updated by: Carlos Navarro
 
@@ -17,7 +17,7 @@ $thisfile = basename(__FILE__, '.php');
 register_plugin(
   $thisfile,
   'News Manager',
-  '2.4.2 beta',
+  '2.4.2',
   'Rogier Koppejan, Carlos Navarro',
   'http://www.cyberiada.org/cnb/news-manager/',
   'A blog/news plugin for GetSimple',
@@ -129,4 +129,3 @@ function nm_frontend_init() {
 function nm_site($content) {
   return '[deprecated]';
 }
-
