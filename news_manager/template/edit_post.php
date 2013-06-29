d7df7fc061c63804a2869785585a9406a7944d49
cnb <carnav@gmail.com>
2013-06-29 16:25:35 +0200

    link to filebrowser (next to image input) #20
    some jQuery code taken from I18N Custom Fields

--- news_manager/template/edit_post.php
+++ news_manager/template/edit_post.php
@@ -11,16 +11,28 @@ if ($NMIMAGEINPUT === true) {
   $NMIMAGEINPUT = 2;
 } else {
   $NMIMAGEINPUT = intval($NMIMAGEINPUT);
-  if ($NMIMAGEINPUT < 0 || $NMIMAGEINPUT > 4)
-    $NMIMAGEINPUT = 2;
+  if ($NMIMAGEINPUT < 0 || $NMIMAGEINPUT > 4) $NMIMAGEINPUT = 2;
 }
 if ($NMIMAGEINPUT) {
-  $imageinput = '<p>
-  <label for="post-image">'.i18n_r('news_manager/POST_IMAGE').':</label>
-  <input class="text short" id="post-image" name="post-image" type="text" value="'.$image.'" />
-  </p>
-  <div class="clear"></div>
-';
+  global $SITEURL;
+  $imageinput = '  <p>
+      <label for="post-image">'.i18n_r('news_manager/POST_IMAGE').':</label>
+      <input class="text short" id="post-image" name="post-image" type="text" style="width:450px" value="'.$image.'" />
+      <span class="edit-nav"><a href="#" id="browse-image">'.i18n_r('SELECT_FILE').'</a></span>
+    </p>
+    <div class="clear"></div>
+    <script type="text/javascript">'."
+      function fill_image(url) {
+        $('#post-image').val(url);
+      }
+      $(function() { 
+        $('#browse-image').click(function(e) {
+          e.preventDefault();
+          window.open('".$SITEURL."plugins/news_manager/browser/filebrowser.php?func=fill_image&type=images', 'browser', 'width=800,height=500,left=100,top=100,scrollbars=yes');
+        });
+      });
+    </script>  
+";
 } else {
   $imageinput = '<input name="post-image" type="hidden" value="'.$image.'" />
 ';
@@ -91,7 +103,7 @@ if ($NMIMAGEINPUT) {
     <div class="clear"></div>
     <?php if ($NMIMAGEINPUT == 2) echo $imageinput; ?>
   </div>
-  <?php if ($NMIMAGEINPUT == 3) echo $imageinput; ?>
+  <?php if ($NMIMAGEINPUT == 3) echo $imageinput; ?>        
   <p>
     <textarea name="post-content"><?php echo $content; ?></textarea>
   </p>
