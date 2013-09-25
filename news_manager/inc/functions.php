cnb/News_Manager-getsimplecms

commit 65af7c25671487c48b47993b0dadcf7ac4ca2643

minor coding style changes
removing variable parsing, etc.

news_manager/inc/functions.php
@@ -251,7 +251,7 @@ function nm_create_excerpt($content, $url=false) {
       if ($url)
         $content .= '<span class="nm_readmore"><a href="'.$url.'">'.i18n_r('news_manager/READ_MORE').'</a></span>';
     }
-    return "<p>$content</p>";
+    return '<p>'.$content.'</p>';
   }
 }
 
@@ -287,9 +287,9 @@ function nm_sitemap_include() {
   if (strval($page['url']) == $NMPAGEURL) {
     $posts = nm_get_posts();
     foreach ($posts as $post) {
-      $url = nm_get_url('post') . $post->slug;
-      $file = NMPOSTPATH . "$post->slug.xml";
-      $date = makeIso8601TimeStamp(date("Y-m-d H:i:s", filemtime($file)));
+      $url = nm_get_url('post').$post->slug;
+      $file = NMPOSTPATH.$post->slug.xml;
+      $date = makeIso8601TimeStamp(date('Y-m-d H:i:s', filemtime($file)));
       $item = $xml->addChild('url');
       $item->addChild('loc', $url);
       $item->addChild('lastmod', $date);
@@ -333,7 +333,7 @@ function nm_header_include() {
 function nm_display_message($msg, $error=false, $backup=null) {
   if (isset($msg)) {
     if (isset($backup))
-      $msg .= " <a href=\"load.php?id=news_manager&amp;restore=$backup\">" . i18n_r('UNDO') . '</a>';
+      $msg .= ' <a href="load.php?id=news_manager&amp;restore='.$backup.'">'.i18n_r('UNDO').'</a>';
     ?>
     <script type="text/javascript">
       $(function() {

news_manager/inc/posts.php
@@ -11,7 +11,7 @@
  * @action edit or create posts
  */
 function nm_edit_post($slug) {
-  $file = NMPOSTPATH . "$slug.xml";
+  $file = NMPOSTPATH.$slug.'.xml';
   if ($slug != '' && dirname(realpath($file)) != realpath(NMPOSTPATH)) die(''); // path traversal
   # get post data, if it exists
   $data    = @getXML($file);
@@ -26,7 +26,7 @@ function nm_edit_post($slug) {
   include(NMTEMPLATEPATH . 'edit_post.php');
   if (file_exists($file)) {
     $mtime = date(i18n_r('DATE_AND_TIME_FORMAT'), filemtime($file));
-    echo '<small>' . i18n_r('news_manager/LAST_SAVED') . ": $mtime</small>";
+    echo '<small>',i18n_r('news_manager/LAST_SAVED'),': ',$mtime,'</small>';
   }
   include(NMTEMPLATEPATH . 'ckeditor.php');
 }
@@ -53,13 +53,13 @@ function nm_save_post() {
     $slug = nm_create_slug($_POST['post-title']);
     if ($slug == '') $slug = 'post';
   }
-  $file = NMPOSTPATH . "$slug.xml";
+  $file = NMPOSTPATH.$slug.'.xml';
   # do not overwrite other posts
   if (file_exists($file)) {
     $count = 1;
-    $file = NMPOSTPATH . "$slug-$count.xml";
+    $file = NMPOSTPATH.$slug.'-'.$count.'.xml';
     while (file_exists($file))
-      $file = NMPOSTPATH . "$slug-" . ++$count . '.xml';
+      $file = NMPOSTPATH.$slug.'-'.++$count.'.xml';
     $slug = basename($file, '.xml');
   }
   # create undo target if there's a backup available
@@ -101,7 +101,7 @@ function nm_save_post() {
  * @action deletes the requested post
  */
 function nm_delete_post($slug) {
-  $file = "$slug.xml";
+  $file = $slug.'.xml';
   # path traversal?
   if (dirname(realpath(NMPOSTPATH.$file)) != realpath(NMPOSTPATH)) {
     nm_display_message('<b>Error:</b> incorrect path', true); // not translated

news_manager/inc/sidebar.php
@@ -18,7 +18,7 @@ function nm_list_recent() {
     foreach ($posts as $post) {
       $url = nm_get_url('post') . $post->slug;
       $title = stripslashes($post->title);
-      echo "<li><a href=\"$url\">$title</a></li>",PHP_EOL;
+      echo '<li><a href="',$url,'">',$title,'</a></li>',PHP_EOL;
     }
     echo '</ul>',PHP_EOL;
   }
@@ -42,7 +42,7 @@ function nm_list_archives($fmt='') {
         $y = $archive;
         $title = nm_get_date($fmt, mktime(0, 0, 0, 1, 1, $y));
         $url = nm_get_url('archive') . $archive;
-        echo "<li><a href=\"$url\">$title</a></li>",PHP_EOL;
+        echo '<li><a href="',$url,'">',$title,'</a></li>',PHP_EOL;
       }
     } else {
       # monthly
@@ -51,7 +51,7 @@ function nm_list_archives($fmt='') {
         list($y, $m) = str_split($archive, 4);
         $title = nm_get_date($fmt, mktime(0, 0, 0, $m, 1, $y));
         $url = nm_get_url('archive') . $archive;
-        echo "<li><a href=\"$url\">$title</a></li>",PHP_EOL;
+        echo '<li><a href="',$url,'">',$title,'</a></li>',PHP_EOL;
       }
     }
     echo '</ul>',PHP_EOL;
@@ -74,9 +74,9 @@ function nm_list_tags() {
     foreach ($tags as $tag=>$count) {
       $url = nm_get_url('tag') . $tag;
       if ($min < $max && $count/$max > 0.5)
-        echo "<a class=\"large\" href=\"$url\">$tag</a> ";
+        echo '<a class="large" href="',$url,'">',$tag,'</a> ';
       else
-        echo "<a href=\"$url\">$tag</a> ";
+        echo '<a href="',$url,'">',$tag,'</a> ';
     }
   }
 }

news_manager/inc/site.php
@@ -76,7 +76,7 @@ function nm_show_search_results() {
   foreach ($keywords as $keyword) {
     $match = array();
     foreach ($posts as $post) {
-      $data = getXML(NMPOSTPATH . "$post->slug.xml");
+      $data = getXML(NMPOSTPATH.$post->slug.'.xml');
       $content = $data->title . $data->content;
       if (stripos($content, $keyword) !== false)
         $match[] = $post;
@@ -103,7 +103,7 @@ function nm_show_search_results() {
  * @action show the requested post on front-end news page
  */
 function nm_show_post($slug, $excerpt=false, $readmore=false, $titlenolink=false) {
-  $file = NMPOSTPATH . "$slug.xml";
+  $file = NMPOSTPATH.$slug.'.xml';
   if (dirname(realpath($file)) == realpath(NMPOSTPATH)) // no path traversal
     $post = @getXML($file);
   if (!empty($post) && $post->private != 'Y') {
@@ -137,7 +137,7 @@ function nm_show_post($slug, $excerpt=false, $readmore=false, $titlenolink=false
           echo '<a href="',$url,'">',$title,'</a>';
         ?>
       </h3>
-      <p class="nm_post_date"><?php echo i18n_r('news_manager/PUBLISHED') . " $date"; ?></p>
+      <p class="nm_post_date"><?php echo i18n_r('news_manager/PUBLISHED'),' ',$date; ?></p>
       <?php if ($image) { 
       ?><p class="nm_post_image"><img src="<?php echo htmlspecialchars($image); ?>"<?php echo $imgattr; ?> /></p>
       <?php } ?>
@@ -156,7 +156,7 @@ function nm_show_post($slug, $excerpt=false, $readmore=false, $titlenolink=false
       }
       
       # single post page?
-      if (strstr($_SERVER['QUERY_STRING'], "post=$slug")) {
+      if (strstr($_SERVER['QUERY_STRING'], 'post='.$slug)) {
         # store post title
         global $NMPOSTTITLE;
         $NMPOSTTITLE = $title;
@@ -195,7 +195,7 @@ function nm_show_navigation($index, $total) {
   if ($index > 0) {
     ?>
     <div class="right">
-      <a href="<?php echo ($index > 1) ? $url . ($index-1) : substr($url, 0, -6); ?>">
+      <a href="<?php echo ($index > 1) ? $url.($index-1) : substr($url, 0, -6); ?>">
         <?php i18n('news_manager/NEWER_POSTS'); ?>
       </a>
     </div>

commit 76054dea1ca2b744444af1b737901743081c1b32

fix legacy function
bug introduced in 65af7c25671487c48b47993b0dadcf7ac4ca2643
function no longer used (was for GS 3.0), will probably remove it, but
anyway better fix it.

news_manager/inc/functions.php
@@ -292,7 +292,7 @@ function nm_sitemap_include() {
     $posts = nm_get_posts();
     foreach ($posts as $post) {
       $url = nm_get_url('post').$post->slug;
-      $file = NMPOSTPATH.$post->slug.xml;
+      $file = NMPOSTPATH.$post->slug.'.xml';
       $date = makeIso8601TimeStamp(date('Y-m-d H:i:s', filemtime($file)));
       $item = $xml->addChild('url');
       $item->addChild('loc', $url);