54cff28904d9515abd0f2a97d6d2426a8fca4e2d
cnb <carnav@gmail.com>
2014-06-20 17:52:02 +0200

    3.0 beta 23

--- news_manager.php
+++ news_manager.php
@@ -3,14 +3,14 @@
 /*
 Plugin Name: News Manager
 Description: A blog/news plugin for GetSimple
-Version: 3.0 beta 22b
+Version: 3.0 beta 23
 Original author: Rogier Koppejan
 Updated by: Carlos Navarro
 
 */
 
 # plugin version
-define('NMVERSION', '3.0 beta 22b');
+define('NMVERSION', '3.0 beta 23');
 
 # get correct id for plugin
 $thisfile = basename(__FILE__, '.php');
