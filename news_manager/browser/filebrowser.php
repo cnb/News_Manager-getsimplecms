e92c43685be20d36d2cf3d25a6ccb833dae34d03
cnb <carnav@gmail.com>
2014-12-14 18:53:12 +0100

    fix #179 filebrowser notices

--- news_manager/browser/filebrowser.php
+++ news_manager/browser/filebrowser.php
@@ -63,6 +63,8 @@ $LANG_header = preg_replace('/(?:(?<=([a-z]{2}))).*/', '', $LANG);
 	$totalsize = 0;
 	$filesArray = array();
 	$dirsArray = array();
+	$filesSorted = array();
+	$dirsSorted = array();
 
 	$filenames = getFiles($path);
 	if (count($filenames) != 0) { 
