a93f0b4756c756d00059599f8aba63c85980102c
cnb <carnav@gmail.com>
2015-06-06 11:08:21 +0200

    fix excerpts: code in<style>, <script>, ... #211

--- news_manager/inc/functions.php
+++ news_manager/inc/functions.php
@@ -319,6 +319,38 @@ function nm_create_excerpt($content, $url=false, $forcereadmore=false) {
 function nm_make_excerpt($content, $len=200, $ellipsis='', $break=false) {
   $content = preg_replace('/\(%.*?%\)/', '', $content); // remove (% ... %)
   $content = preg_replace('/\{%.*?%\}/', '', $content); // remove {% ... %}
+  
+  # Remove HTML tags, including invisible text such as style and
+  # script code, and embedded objects.  Add line breaks around
+  # block-level tags to prevent word joining after tag removal.
+  # http://nadeausoftware.com/articles/2007/09/php_tip_how_strip_html_tags_web_page
+  $content = preg_replace(
+    array(
+      // Remove invisible content
+      '@<head[^>]*?>.*?</head>@siu',
+      '@<style[^>]*?>.*?</style>@siu',
+      '@<script[^>]*?.*?</script>@siu',
+      '@<object[^>]*?.*?</object>@siu',
+      '@<embed[^>]*?.*?</embed>@siu',
+      '@<applet[^>]*?.*?</applet>@siu',
+      '@<noframes[^>]*?.*?</noframes>@siu',
+      '@<noscript[^>]*?.*?</noscript>@siu',
+      '@<noembed[^>]*?.*?</noembed>@siu',
+      // Add line breaks before and after blocks
+      '@</?((address)|(blockquote)|(center)|(del))@iu',
+      '@</?((div)|(h[1-9])|(ins)|(isindex)|(p)|(pre))@iu',
+      '@</?((dir)|(dl)|(dt)|(dd)|(li)|(menu)|(ol)|(ul))@iu',
+      '@</?((table)|(th)|(td)|(caption))@iu',
+      '@</?((form)|(button)|(fieldset)|(legend)|(input))@iu',
+      '@</?((label)|(select)|(optgroup)|(option)|(textarea))@iu',
+      '@</?((frameset)|(frame)|(iframe))@iu',
+    ),
+    array(
+      ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+      "\n\$0", "\n\$0", "\n\$0", "\n\$0", "\n\$0", "\n\$0",
+      "\n\$0", "\n\$0",
+    ), $content);
+
   $content = strip_tags($content);
   $content = preg_replace('/\s+/u', ' ', str_replace('&nbsp;', ' ', $content)); // remove whitespace
   if (function_exists('mb_strlen')) {
