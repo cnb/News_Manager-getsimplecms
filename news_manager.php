b2337d58eef68af8111ab63e61aad40315e9df5d
cnb <carnav@gmail.com>
2016-02-11 20:37:16 +0100

    3.3 beta 30

--- news_manager.php
+++ news_manager.php
@@ -10,7 +10,7 @@ Updated by: Carlos Navarro
 */
 
 # plugin version
-define('NMVERSION', '3.3 beta 29');
+define('NMVERSION', '3.3 beta 30');
 
 # get correct id for plugin
 $thisfile = basename(__FILE__, '.php');
